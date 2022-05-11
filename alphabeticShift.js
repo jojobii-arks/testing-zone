7275663772e70b97fa91a302cecdb07ac105a975
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-11 12:54:48 -0700

    completed alphabet shift

--- alphabeticShift.js
+++ alphabeticShift.js
@@ -1,8 +1,16 @@
 function solution(inputString) {
-
+	const array = inputString.split('');
+	const newArray = array
+		.map(e => e.charCodeAt(0))
+		.map(e => {
+			e++;
+			if (e > 122) e = 97;
+			return String.fromCharCode(e);
+		});
+	return newArray.join('');
 }
 
-const a = solution("crazy");
+const a = solution('crazy');
 console.log(a);
-const b = solution("fuzzy");
+const b = solution('fuzzy');
 console.log(b);
